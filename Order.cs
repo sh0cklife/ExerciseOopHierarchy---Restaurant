0111ff4b71460797a6220ebd6a5ab227519f96fe
Denis Atanassov <134636754+sh0cklife@users.noreply.github.com>
2023-12-13 20:22:27 +0200

    Add project files.

---
+++ Order.cs
@@ -0,0 +1,22 @@
+﻿using ExerciseOopHierarchy;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExerciseOopHierarchy;
+
+public class Order
+{
+    private List<MenuItem> _items= new ();
+    public IReadOnlyCollection<MenuItem> Items => this._items.AsReadOnly();
+
+    public void AddItem(MenuItem item)
+    {
+        this._items.Add(item);
+    }
+
+    public decimal GetTotal()
+    {
+        return this._items.Select(i => i.Price).Sum();
+    }
+   
+}
